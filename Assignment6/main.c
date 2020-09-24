a0681b6845f36e97ebce4d05ea4e5b18b207d09c
Lamis Tarek <69652859+LamisTarek@users.noreply.github.com>
2020-09-24 21:37:22 +0200

    AS6

---
+++ Assignment6/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+void ArrayPrint(int *Arr, int size );
+int main ()
+{
+	int Arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+	int Size = sizeof(Arr)/sizeof(Arr[0]);
+	ArrayPrint(Arr,Size);
+	
+
+
+
+}
+
+
+void ArrayPrint(int *Arr, int size )
+{
+	int i ; 
+	for(i=0;i<size;i++)
+	{
+		printf("%d	",Arr[i]);
+	}
+	printf("\n");
+
+}
